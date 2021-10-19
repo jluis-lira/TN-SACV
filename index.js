aa8926a2f4bc2135828b80e09eaf86bd6f19745b
jluis-lira <js.black.green@gmail.com>
2021-10-18 23:25:07 -0500

    Configuring Middleware and Routes

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ const express = require('express');
 const app = express();
 //Routers
 const user = require('./Routes/user');
+const employee = require('./routes/employee');
+//Middleware
+const auth = require('./middleware/auth');
+const notFound = require('./middleware/notFound');
+const index = require('./middleware/index');
+const cors = require('./middleware/cors');
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-//app.get("/",index);
+app.get("/",index);
 app.use("/user",user);
+app.use(auth);
+app.use("/employee",employee);
 
 app.listen(process.env.PORT||3000,()=>{
     console.log("Server is running..");
